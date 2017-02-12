df2e6947b8d7a0b44d117be1496a102ec7d0de6e
Yaco Ferrero <jferrero@whynotsoluciones.com>
2017-02-12 18:44:23 +0100

    Initial commit: basic skeleton running

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=insided1',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
